0c1f5e8452bfe44b011b5aea0f603ee5a746e1f7
SamkHarks <samikh90@gmail.com>
2024-01-10 23:08:08 +0200

    add controller and view to main

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@
 #include "card.hpp"
 #include "player.hpp"
 #include "blackjackgame.hpp"
+#include "blackjackcontroller.hpp"
 
 int main(void) {
     Player player("Player");
     Player dealer("Dealer");
     BlackJackGame game(player, dealer);
+    BlackJackView view;
+    BlackJackController controller(game, view);
+    controller.playGame();
     game.play();
     return 0;
 }
